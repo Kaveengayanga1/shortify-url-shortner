0f3c92a3089602180e5ff1e0bac69ff1d8f3fc2a
kaveengayanga1 <kaveengayanga@gmail.com>
2026-06-14 08:32:12 +0530

    feat(lambda): enable tracing for create and redirect functions

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -10,6 +10,7 @@ module "create_fn" {
   source_dir       = "${path.root}/../../app/src/create"
   table_arn        = module.dynamodb.table_arn
   dynamodb_actions = ["dynamodb:PutItem"]
+  enable_tracing = true
   environment = {
     TABLE_NAME = module.dynamodb.table_name
   }
@@ -21,6 +22,7 @@ module "redirect_fn" {
   source_dir       = "${path.root}/../../app/src/redirect"
   table_arn        = module.dynamodb.table_arn
   dynamodb_actions = ["dynamodb:GetItem", "dynamodb:UpdateItem"]
+  enable_tracing = true
   environment = {
     TABLE_NAME = module.dynamodb.table_name
   }
